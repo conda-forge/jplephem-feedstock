afe109e01e335637a6ed64815543e61ded1ba716
Paweł T. Jochym <jochym@users.noreply.github.com>
2018-07-10 12:16:02 +0200

    Adapt meta closer to noarch package (#11)
    * Adapt meta closer to noarch package
    * Drop obsolate bld/test/scripts
    * Add curl to test requirements
    * Make curl less verbose

--- recipe/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -O https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de421.bsp
-curl -O https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de430.bsp
-curl -O https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de405.bsp
-curl -O ftp://ssd.jpl.nasa.gov/pub/eph/planets/test-data/430/testpo.430
-
-python -m unittest discover jplephem.test
-python -m jplephem.jpltest
